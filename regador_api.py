41980f3be4e31c7fe7fea9e6f9b1cb17cc13aa32
binkssake2 <binkssake2@gmail.com>
2020-03-08 18:10:24 -0300

    nono

--- regador_api.py
+++ regador_api.py
@@ -24,7 +24,7 @@ def deletar(evento):
     requests.put(url, json=data, headers=headers)
     return "<h1>Evento deletado<hi>"
 
-@app.route('/criar/<str:evento')
+@app.route('/criar/<str:evento>')
 def criar(evento):
     url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
     headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
