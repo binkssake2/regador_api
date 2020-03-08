daf696cb0b56e949f59445469a29b5dc66bdd1e2
binkssake2 <binkssake2@gmail.com>
2020-03-08 17:39:31 -0300

    commit inicial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python regador_api.py
---
+++ regador_api.py
@@ -0,0 +1,53 @@
+import os
+import requests
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Escreva /criar/evento para cria-lo e /deletar/evento para deletar, o evento é da forma 00:00/05 onde se tem o inicio e sua duracao em minutos (botar o 0 quando na tiver nos minutos)</hi>"
+
+
+@app.route('/deletar/<str:evento>')
+def deletar(evento):
+
+    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
+    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
+
+    req = requests.get(url, headers=headers)
+    infos = req.json()["infos"]
+    infos = infos.replace(evento + "," ,"")
+    
+    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/'
+    headers = {'Content-Type': 'application/json','secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
+    data = {"infos":infos}
+    requests.put(url, json=data, headers=headers)
+    
+	return redirect("<h1>Evento deletado<h1>")
+
+@app.route('/criar/<srt:evento')
+def criar(evento):
+
+    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
+    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
+
+    req = requests.get(url, headers=headers)
+    infos = req.json()["infos"]
+    infos = infos + "," + evento +","
+    
+    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/'
+    headers = {'Content-Type': 'application/json','secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
+    data = {"infos":infos}
+    requests.put(url, json=data, headers=headers)
+
+    return redirect("<h1>Evento criado<h1>")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
