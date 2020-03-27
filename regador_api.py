11016f490e40203e1687d65e68e43327db66aad6
unknown <rafaelsbrito21@gmail.com>
2020-03-26 21:43:22 -0300

    beauty_api

--- regador_api.py
+++ regador_api.py
@@ -3,9 +3,28 @@ import requests
 import json
 from flask import Flask, render_template, request, redirect
 
+api_url = 'https://api.jsonbin.io/b'
+bin_ID = '5e7d46aa862c46101abec6c9'
+secret_key = '$2b$10$sqXNBeWaUKx1wfpvNgpv.u4xUF.nUWVH4GIgZRqnaI30vibIYPv4.'
+
+def read_from_db():
+    url = f'{api_url}/{bin_ID}/latest'
+    headers = {'secret-key': secret_key}
+    req = requests.get(url, headers=headers)
+    return req.json()
+
+def overwrite_db(infos):
+    url = f'{api_url}/{bin_ID}/'
+    headers = {
+        'Content-Type': 'application/json',
+        'secret-key': secret_key,
+        'versioning':False
+    }
+    requests.put(url, json=infos, headers=headers)
 
 def createeventoshtml(eventos):
     html = "<h1>Todos os eventos</h1>"
+
     for idx, evento in enumerate(eventos):
         eventohtml = f'<h3>Evento {idx}'
         iniciohtml = f'<ul><li>Hora de início: {evento[0][0]}:{evento[0][1]}</li>'
@@ -13,8 +32,10 @@ def createeventoshtml(eventos):
         removehtml = f'<li><a href="/deletar/{idx}">Deletar</a></li></ul>'
         closehtml = "</h3>"
         html += (eventohtml + iniciohtml + durhtml + removehtml + closehtml)
+
     htmlcriar = '<a href="/criar">Criar novo evento</a>'
     html += htmlcriar
+
     return html
 
 
@@ -23,19 +44,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Olá, essa é a aplicação que controla a rega das plantas!</hi><p>Para fins de usuários temos apenas uma rota de interesse, que é a que controla os eventos. Para ir até ela basta clicar no link abaixo:</p><a href='/eventos'>Ir para eventos</a>"
+    return "<h1>Olá, essa é a aplicação que controla a rega das plantas!</hi>" \
+    "<p>Para fins de usuários temos apenas uma rota de interesse, que é a que" \
+    "controla os eventos. Para ir até ela basta clicar no link abaixo:</p>" \
+    "<a href='/eventos'>Ir para eventos</a>"
 
 
 @app.route('/deletar/<int:eventID>')
 def deletar(eventID):
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
-    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    req = requests.get(url, headers=headers)
-    infos = req.json()
+    infos = read_from_db()
     infos.pop(eventID)
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/'
-    headers = {'Content-Type': 'application/json','secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    requests.put(url, json=infos, headers=headers)
+    overwrite_db(infos)
+
     return redirect("/eventos", code=302)
 
 @app.route('/criar')
@@ -54,43 +74,32 @@ def criar():
 def handlecriar():
     hora = request.args.get('hora').split(':')
     dur = request.args.get('dur').split(':')
+
     evento = [
         [
-            int(hora[0]), 
+            int(hora[0]),
             int(hora[1])
         ],
         [
-            int(dur[0]), 
+            int(dur[0]),
             int(dur[1])
         ]
     ]
 
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
-    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    req = requests.get(url, headers=headers)
-    db = req.json()
-
+    db = read_from_db()
     db.append(evento)
+    overwrite_db(db)
 
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/'
-    headers = {'Content-Type': 'application/json','secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    requests.put(url, json=db, headers=headers)
     return redirect("/eventos", code=302)
 
 @app.route('/eventos')
 def eventos():
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
-    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    req = requests.get(url, headers=headers)
-    infos = req.json()
+    infos = read_from_db()
     return createeventoshtml(infos)
 
 @app.route('/db')
 def dbview():
-    url = 'https://api.jsonbin.io/b/5e61133074ed8a66ce71e657/latest'
-    headers = {'secret-key': '$2b$10$ZYd/uxhp./Hfer6e/nPFW.iAhykkM2rGWMSbO2St8K2YEh7NUc8Z2'}
-    req = requests.get(url, headers=headers)
-    infos = req.json()
+    infos = read_from_db()
     return json.dumps(infos)
 
 
@@ -98,4 +107,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-    
+
